ead1932187798a82a04d0d0fd7d03fcb06c122df
rohit170702 <162716651+rohit170702@users.noreply.github.com>
2024-03-10 18:35:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const carouselContainer = document.querySelector('.carousel-container');
+const carouselItems = document.querySelectorAll('.carousel-item');
+const leftBtn = document.querySelector('.control-btn.left');
+const rightBtn = document.querySelector('.control-btn.right');
+
+let currentSlide = 0; 
+
